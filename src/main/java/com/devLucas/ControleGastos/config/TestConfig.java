7b25817d88eb662588c263d2666dd5d8129089bd
Lucas Mussatto Rodrigues <lucasmussatto71@gmail.com>
2025-11-27 20:45:22 -0300

    Ajustes no sistema

--- src/main/java/com/devLucas/ControleGastos/config/TestConfig.java
+++ src/main/java/com/devLucas/ControleGastos/config/TestConfig.java
@@ -36,59 +36,66 @@ public class TestConfig implements CommandLineRunner {
 		if (valor == 1) {
 			System.out.println("Bem vindo ao sistema de contas a pagar! ");
 			System.out.println("------------------------------------");
-			System.out.println("Qual área deseja acessar?"
-					+ "1 - INCLUSÃO DE CONTA A PAGAR"
-					+ "2 - VISUALIZAÇÃO DE CONTAS A PAGAR"
-					+ "3 - MODIFICALÇAO DE TÍTULO");
-			System.out.println("Quantas contas deseja incluir? ");
-			int valorDigitado = sc.nextInt();
-			sc.nextLine();
+			System.out.println("Qual área deseja acessar? \n" + "1 - INCLUSÃO DE CONTA A PAGAR\n"
+					+ "2 - VISUALIZAÇÃO DE TÍTULOS\n" + "3 - MODIFICAÇÃO DE TÍTULO\n");
+			int valorDigi = sc.nextInt();
 
-			for (int i = 0; i < valorDigitado; i++) {
-				System.out.println("Digite o nome do título: ");
-				String name = sc.nextLine();
-
-				System.out.println("Digite o valor do título a pagar: ");
-				double amount = sc.nextDouble();
+			switch (valorDigi) {
+			case 1:
+				System.out.println("Quantas contas deseja incluir? ");
+				int valorDigitado = sc.nextInt();
 				sc.nextLine();
 
-				System.out.println("Digite a data do vencimento do título (dd/MM/yyyy): ");
-				String x = sc.nextLine();
+				for (int i = 0; i < valorDigitado; i++) {
+					System.out.println("Digite o nome do título: ");
+					String name = sc.nextLine();
 
-				LocalDate dueDate = null;
-				try {
-					dueDate = LocalDate.parse(x, fmt);
-				} catch (DateTimeParseException e) {
-					System.out.println("Data inválida! Use o formato dd/MM/yyyy.");
-					return;
+					System.out.println("Digite o valor do título a pagar: ");
+					double amount = sc.nextDouble();
+					sc.nextLine();
+
+					System.out.println("Digite a data do vencimento do título (dd/MM/yyyy): ");
+					String x = sc.nextLine();
+
+					LocalDate dueDate = null;
+					try {
+						dueDate = LocalDate.parse(x, fmt);
+					} catch (DateTimeParseException e) {
+						System.out.println("Data inválida! Use o formato dd/MM/yyyy.");
+						return;
+					}
+
+					System.out.println("Digite o nome da loja: ");
+					String store = sc.nextLine();
+
+					System.out.println("Escolha a situação do título: ");
+					for (currentSituation s : currentSituation.values()) {
+						System.out.println("- " + s);
+					}
+
+					String situationStr = sc.nextLine();
+
+					currentSituation situation;
+					try {
+						situation = currentSituation.valueOf(situationStr.toUpperCase());
+					} catch (IllegalArgumentException e) {
+						System.out.println("Situação inválida! Use: PENDENTE, PAGO ou ATRASADO.");
+						return;
+					}
+
+					AccountsPayable accounts = new AccountsPayable(null, name, amount, dueDate, store, situation);
+
+					accountsRepository.saveAll(Arrays.asList(accounts));
 				}
 
-				System.out.println("Digite o nome da loja: ");
-				String store = sc.nextLine();
-
-				System.out.println("Escolha a situação do título: ");
-
-				for (currentSituation s : currentSituation.values()) {
-					System.out.println("- " + s);
-				}
-
-				String situationStr = sc.nextLine();
-
-				currentSituation situation;
-				try {
-					situation = currentSituation.valueOf(situationStr.toUpperCase());
-				} catch (IllegalArgumentException e) {
-					System.out.println("Situação inválida! Use: PENDENTE, PAGO ou ATRASADO.");
-					return;
-				}
-
-				AccountsPayable accounts = new AccountsPayable(null, name, amount, dueDate, store, situation);
-
-				accountsRepository.saveAll(Arrays.asList(accounts));
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			default:
+				System.out.println("Digite um valor correto!");
 			}
-
-		} else {
-			System.out.println("Digite o valor correto!");
 		}
 	}
 }
